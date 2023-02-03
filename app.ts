977b84308eaf0942aa55e3361ccba4bf331706b6
xerxi88 <serjicu88@gmail.com>
2023-02-03 21:11:04 +0100

    Nivell 1 Exercici 1

---
+++ app.ts
@@ -0,0 +1,22 @@
+const requestApiJoke:any = async ()=>{
+
+    const respuesta = await fetch('https://icanhazdadjoke.com/slack');
+    const data = await respuesta.json();
+
+    console.log(data.attachments[0].text);
+
+    let caja: any = document.getElementById("jokeBox");
+    caja.textContent = data.attachments[0].text;
+}
+
+let boton:any = document.getElementById("next");
+boton?.addEventListener('click',()=> {requestApiJoke()});
+
+
+
+
+
+
+
+
+
